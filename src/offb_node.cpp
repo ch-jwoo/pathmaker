fc2bb7ec3899bd94b06b69524a5eecce42d56f8e
yeojunni <yeojun0702@gmail.com>
2020-01-30 23:47:24 +0900

    keyboard telop + att_ctrl

--- src/offb_node.cpp
+++ src/offb_node.cpp
@@ -4,6 +4,11 @@
  * Stack and tested in Gazebo SITL
  */
 #include <ros/ros.h>
+#include <std_msgs/String.h> 
+#include <stdio.h>
+#include "geometry_msgs/Vector3Stamped.h"
+#include "std_msgs/Float64.h"
+
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <mavros_msgs/CommandBool.h>
@@ -86,11 +91,13 @@ int main(int argc, char **argv)
             ("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
-
+    ros::Publisher pub_att = nh.advertise<geometry_msgs::PoseStamped>
+            ("/mavros/setpoint_attitude/attitude",10);
     ros::Subscriber keyInput = nh.subscribe<pathmaker::key>
             ("keyboardInput/key", 10, state_key);
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>
             ("mavros/setpoint_velocity/cmd_vel_unstamped", 10);
+    ros::Publisher pub_thr = nh.advertise<std_msgs::Float64>("/mavros/setpoint_attitude/att_throttle", 10);
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
@@ -99,12 +106,28 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
     }
-
-
-    geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 2;
+     geometry_msgs::PoseStamped pose;
+     pose.pose.position.x = 0;
+     pose.pose.position.y = 0;
+     pose.pose.position.z = 2;
+     geometry_msgs::PoseStamped cmd_att;
+     double v[3]={1.0, 0.0, 0.0};
+     double v_norm=sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+     double theta=0.0;
+     double lambda = 0.3;
+     int count = 1;
+     std_msgs::Float64 cmd_thr;
+     cmd_att.header.stamp = ros::Time::now();
+     cmd_att.header.seq=count;
+     cmd_att.header.frame_id = 1;
+     cmd_att.pose.position.x = 0.0;//0.001*some_object.position_x;
+     cmd_att.pose.position.y = 0.0;//0.001*some_object.position_y;
+     cmd_att.pose.position.z = 5.0;//0.001*some_object.position_z;
+     cmd_att.pose.orientation.x = sin(theta/2.0)*v[0]/v_norm;
+     cmd_att.pose.orientation.y = sin(theta/2.0)*v[1]/v_norm;
+     cmd_att.pose.orientation.z = sin(theta/2.0)*v[2]/v_norm;
+     cmd_att.pose.orientation.w = cos(theta/2.0);
+     
   /* if(
 depth<5)
 {
@@ -112,11 +135,11 @@ depth<5)
     pose.pose.position.y=-5;
 }*/
     //send a few setpoints before starting
-    for(int i = 100; ros::ok() && i > 0; --i){
+    /*for(int i = 100; ros::ok() && i > 0; --i){
         local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
-    }
+    }*/
 
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
@@ -137,6 +160,7 @@ depth<5)
         } else {
             if( !current_state.armed &&
                 (ros::Time::now() - last_request > ros::Duration(5.0))){
+                    ROS_INFO("trying to arm....");
                 if( arming_client.call(arm_cmd) &&
                     arm_cmd.response.success){
                     ROS_INFO("Vehicle armed");
@@ -144,10 +168,13 @@ depth<5)
                 last_request = ros::Time::now();
             }
         }
-
-        //local_pos_pub.publish(pose);
-        vel_pub.publish(vel);
-        ros::spinOnce();
+   	    //cmd_thr.data = lambda;
+        //pub_att.publish(cmd_att);
+        //pub_thr.publish(cmd_thr);
+        //theta=0.3*sin(count/300.0);
+        //count++;
+        //vel_pub.publish(vel);
+        //ros::spinOnce();
         rate.sleep();
     }
 
