7ac5277ab4e677393ae0010b93f380b98f9f5eb3
yeojunni <yeojun0702@gmail.com>
2020-01-31 16:36:01 +0900

    bugfix

--- src/offb_node.cpp
+++ src/offb_node.cpp
@@ -1,163 +1,86 @@
-/**
- * @file offb_node.cpp
- * @brief Offboard control example node, written with MAVROS version 0.19.x, PX4 Pro Flight
- * Stack and tested in Gazebo SITL
- */
 #include <ros/ros.h>
 #include <std_msgs/String.h> 
 #include <stdio.h>
+#include <math.h>
+#include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Vector3Stamped.h"
-#include "std_msgs/Float64.h"
-
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Twist.h>
+#include "mavros_msgs/Thrust.h"
+#include "std_msgs/Float32.h"
+#include "mavros/mavros.h"
+#include <cmath>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
-#include "pathmaker/key.h"
-#include "pathmaker/keyboard.h"
-#include <math.h>
-//#include <std_msgs/Float32.h>
-
-
-
-mavros_msgs::State current_state;
-pathmaker::key current_key;
-geometry_msgs::Twist vel;
-//float depth=0;
-void calSpeed(double x, double y, double z, double rz){
-    vel.linear.x = x;
-    vel.linear.y = y;
-    vel.linear.z = z;
-    vel.angular.z = rz;
-}
-
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
+ mavros_msgs::State current_state;
+//#include </home/mahesh/catkin_ws/src/beginner_tutorials/src/Qualisys.h>
+ void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
-void state_key(const pathmaker::key::ConstPtr& msg){
-    current_key = *msg;
-    switch(msg->key){
-        case keyboardInput::W: // W, S : alttitude vel.linear.z
-            calSpeed(0,0,5,0);
-            break;
-        case keyboardInput::S:
-            calSpeed(0,0,-5,0);
-            break;
-        case keyboardInput::A: // A, D : YAW vel.angular.z
-            calSpeed(0,0,0,5);
-            break;
-        case keyboardInput::D:
-            calSpeed(0,0,0,-5);
-            break;
-        case keyboardInput::UP: // UP, DOWN : vel.linear.x
-            calSpeed(5,0,0,0);
-            break;
-        case keyboardInput::DOWN:
-            calSpeed(-5,0,0,0);
-            break;
-        case keyboardInput::LEFT: // LEFT, RIGHT : vel.linear.y
-            calSpeed(0,5,0,0);
-            break;
-        case keyboardInput::RIGHT:
-            calSpeed(0,-5,0,0);
-            break;
-        default:
-            calSpeed(0,0,0,0);
-            break;
-    }
-
-}
-
-/*void dep(const std_msgs::Float32::ConstPtr& msg)
-{
-  depth=msg->data;
-ROS_INFO("%f",depth);
-}
-*/
 
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "offb_node");
-    ros::NodeHandle nh;
-//    ros::Subscriber getdata=nh.subscribe<std_msgs::Float32>("/float32",10, dep);
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+   ros::init(argc, argv, "offb_node");
+   ros::NodeHandle n;
+   ros::Subscriber state_sub = n.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-           ("mavros/setpoint_position/local", 10);
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+   ros::Publisher pub_att = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_attitude/attitude",10);
+   ros::Publisher pub_thr = n.advertise<mavros_msgs::Thrust>("/mavros/setpoint_attitude/thrust", 10);
+   //ros::Publisher param_set=n.advertise<mavros_msgs::ParamSet>("/mavros/param/push")
+   ros::ServiceClient arming_client = n.serviceClient<mavros_msgs::CommandBool>
             ("mavros/cmd/arming");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+    ros::ServiceClient set_mode_client = n.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
-    ros::Publisher pub_att = nh.advertise<geometry_msgs::PoseStamped>
-            ("/mavros/setpoint_attitude/attitude",10);
-    ros::Subscriber keyInput = nh.subscribe<pathmaker::key>
-            ("keyboardInput/key", 10, state_key);
-    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>
-            ("mavros/setpoint_velocity/cmd_vel_unstamped", 10);
-    ros::Publisher pub_thr = nh.advertise<std_msgs::Float64>("/mavros/setpoint_attitude/att_throttle", 10);
-    //the setpoint publishing rate MUST be faster than 2Hz
-    ros::Rate rate(20.0);
+   ros::Rate loop_rate(100);
+   
+   
+   geometry_msgs::PoseStamped cmd_att;
+   mavros_msgs::Thrust cmd_thr;
+   //mavros_msgs::ParamSet cmd_param;
+   std_msgs::Header head;
+   mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+   mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+   
+   ros::Time last_request = ros::Time::now();
 
-    // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
-        ros::spinOnce();
-        rate.sleep();
-    }
-     geometry_msgs::PoseStamped pose;
-     pose.pose.position.x = 0;
-     pose.pose.position.y = 0;
-     pose.pose.position.z = 2;
-     geometry_msgs::PoseStamped cmd_att;
-     double v[3]={1.0, 0.0, 0.0};
-     double v_norm=sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
-     double theta=0.0;
-     double lambda = 0.3;
-     int count = 1;
-     std_msgs::Float64 cmd_thr;
+  
+   int count = 1;
+   double v[3]={0.0, 0.0, 1.0};
+   double lambda = 0.45;
+   head.stamp = ros::Time::now();
+   head.seq = count;
+   head.frame_id = 1;
+   
+   double theta=1.0; 
+   double v_norm=sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+   //double theta=0.0;
+     cmd_thr.header = head;
+     cmd_thr.thrust = lambda;
      cmd_att.header.stamp = ros::Time::now();
      cmd_att.header.seq=count;
      cmd_att.header.frame_id = 1;
      cmd_att.pose.position.x = 0.0;//0.001*some_object.position_x;
      cmd_att.pose.position.y = 0.0;//0.001*some_object.position_y;
-     cmd_att.pose.position.z = 5.0;//0.001*some_object.position_z;
+     cmd_att.pose.position.z = 0.0;//0.001*some_object.position_z;
+ 
      cmd_att.pose.orientation.x = sin(theta/2.0)*v[0]/v_norm;
      cmd_att.pose.orientation.y = sin(theta/2.0)*v[1]/v_norm;
      cmd_att.pose.orientation.z = sin(theta/2.0)*v[2]/v_norm;
      cmd_att.pose.orientation.w = cos(theta/2.0);
-     
-  /* if(
-depth<5)
-{
-    pose.pose.position.x=-5;
-    pose.pose.position.y=-5;
-}*/
-    //send a few setpoints before starting
-    /*for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(pose);
-        ros::spinOnce();
-        rate.sleep();
-    }*/
-
-    mavros_msgs::SetMode offb_set_mode;
-    offb_set_mode.request.custom_mode = "OFFBOARD";
-
-    mavros_msgs::CommandBool arm_cmd;
-    arm_cmd.request.value = true;
-
-    ros::Time last_request = ros::Time::now();
-
+           
     while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
+        /*if( current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
             if( set_mode_client.call(offb_set_mode) &&
                 offb_set_mode.response.mode_sent){
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
-        } else {
+            }
+         else {
             if( !current_state.armed &&
                 (ros::Time::now() - last_request > ros::Duration(5.0))){
                     ROS_INFO("trying to arm....");
@@ -166,17 +89,26 @@ depth<5)
                     ROS_INFO("Vehicle armed");
                 }
                 last_request = ros::Time::now();
-            }
+                }*/
+        
+       pub_att.publish(cmd_att);
+       pub_thr.publish(cmd_thr);      
+       
+       ros::spinOnce();
+        count++;
+	   theta=0.3*(count/300.0);
+       ROS_INFO("deg: %f",theta);
+       loop_rate.sleep();
         }
-   	    //cmd_thr.data = lambda;
-        //pub_att.publish(cmd_att);
-        //pub_thr.publish(cmd_thr);
-        //theta=0.3*sin(count/300.0);
-        //count++;
-        //vel_pub.publish(vel);
-        //ros::spinOnce();
-        rate.sleep();
-    }
+        
+       
+       
 
-    return 0;
+ return 0;
+        
 }
+
+   
+    
+       
+  
